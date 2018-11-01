a9f11a8b43147449955fd70dcca78008f7795338
cabooshy <cambomiller@googlemail.com>
2018-11-01 14:26:43 +0000

    Add the start of vert's code

---
+++ bot.py
@@ -0,0 +1,12 @@
+import datetime
+import sys
+import traceback
+import discord
+from discord.ext import commands
+from botconfig import token
+from botconfig import prefix
+from botconfig import botdesc
+from botconfig import botversion
+from botconfig import since
+from botconfig import owner
+
---
+++ botconfig-example.py
@@ -0,0 +1,12 @@
+# This is named the way it is so it doesn't conflict with an
+# existing installation, and serves as a way to showcase the layout of this botconfig.py file
+# Rename this file to botconfig.py to use it and remove these comments
+import datetime
+
+botversion = 'The version of the bot'
+since = datetime.datetime('Year', 'Month', 'Day')
+prefix = 'Bot Prefix'
+token = 'Your Bot App Token'
+owner = 'Your Account ID'
+
+botdesc = ''' The Description of what the bot does. '''
